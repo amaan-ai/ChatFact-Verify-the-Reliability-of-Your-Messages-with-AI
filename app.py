68b6de1da3fce9560619ecc018e8b49f3c6d0f9a
amaan-ai <amaanhaque97@gmail.com>
2023-04-04 20:40:40 +0800

    updated description

--- app.py
+++ app.py
@@ -49,6 +49,19 @@ github_link = """
 """
 st.markdown(github_link, unsafe_allow_html=True)
 
+# Add project description
+project_description = """
+<div style='background-color: #c1e1d1; padding: 20px; border-radius: 10px; text-align: justify;'>
+
+The proliferation of messaging apps has made it easier than ever to share information with friends and family. However, the downside to this convenience is the spread of misinformation. False information can quickly spread and lead to unnecessary panic and confusion. In some cases, it can even have serious consequences.
+
+To combat this problem, it is important to have a reliable tool to check the veracity of forwarded messages. That's where this application comes in. By leveraging the power of AI and natural language processing, it can quickly determine whether a message is credible or not.
+
+The motivation behind this project is to provide a simple and accessible solution to help people fact-check the information they receive on messaging apps. By promoting responsible information sharing, we can help prevent the spread of false information and ensure that people have access to accurate and reliable information.
+</div>
+"""
+
+st.markdown(project_description, unsafe_allow_html=True)
 
 tab1, tab2 = st.tabs(["FactCheck", "Help"])
 with tab1:
