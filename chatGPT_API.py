a7e73a6e15ea4465c60a93b5fd6d63788178af0b
haque099 <amaanhaque.sg@gmail.com>
2023-08-05 00:25:10 +0800

    updated input prompt

--- chatGPT_API.py
+++ chatGPT_API.py
@@ -24,8 +24,12 @@ class checkMessage:
     
     def callChatGPT(self, input_text):
         
-        input_prompt = "I have receive the message following message: " + '"' + str(input_text)  + '"'  + "."
-        input_prompt = input_prompt + " \n Can you check if the message is factually correct? \n Please give numerical point wise answer for long messages and do not repeat the original message in your response. \n Keep your response on points concise. \n Make sure you're giving fact check results and nothing else"
+        #input_prompt = "I have receive the message following message: " + '"' + str(input_text)  + '"'  + "."
+        input_prompt = "I have receive the following forwared message of whatsapp which I have enclosed within triple backsticks " + '```' + str(input_text)  + '```'  + "."
+        
+        #input_prompt = input_prompt + " \n Can you check if the message is factually correct? \n Please give numerical point wise answer for long messages and do not repeat the original message in your response. \n Keep your response on points concise. \n Make sure you're giving fact check results and nothing else"
+        input_prompt = input_prompt + " \nNow, Can you please check if the message is factually correct?. \nImportant note: Always consider the following points while giving your response: \n 1) Always give numerical point wise answer for long messages \n 2) Never repeat the original message in your response. \n 3) Keep your response on point and concise. \n 4) Make sure you're giving fact check results and nothing else. \n 5) Finally based on your fact check, If the message contains all correct facts then give this exact message at the end: " + '"' + 'All facts are correct! The message can be forwarded' + '"' + '.' + ' Else incase the message contains factually incorrect information, then give this exact message in the end: '  + '"' + "WARNING: Please do not forward this message, as it contains incorrect information!" + '"' + "." + "\nPlease take your time and do not rush for results." 
+        
         input_prompt = str(input_prompt)
         print("Input prompt is: ", input_prompt)
 
